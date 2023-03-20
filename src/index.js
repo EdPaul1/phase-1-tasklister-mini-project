481a6e8a92c4fdf54daac39175c130af2239ae4a
EdPaul1 <saidarajab101@gmail.com>
2023-03-20 16:44:02 +0300

    TaskLister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    toDo(e.target.new_task_description.value)
+    form.reset()
+  })
 });
+function toDo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', deleteEntry)
+  btn.textContent = 'x'
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#list').appendChild(p)
+}
+function deleteEntry(e){
+  e.target.parentNode.remove()
+}
